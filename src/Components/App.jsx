56cc52afc0369808b2e997536295ec995d6f7e2e
Nikitha-bommagani03 <nikithabommagani03@gmail.com>
2024-09-28 16:06:32 +0530

    data-table

---
+++ src/Components/App.jsx
@@ -0,0 +1,9 @@
+import {Component} from "react"
+import Table from "./Table.jsx"
+export default class App extends Component{
+    render(){
+      return(
+          <div id="container"><Table></Table></div>
+      )
+    }
+}
